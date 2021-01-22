c2138c16bc394481b0b6a84d060c543a5e232247
Vince R <r2d2fan20@outlook.com>
2021-01-22 00:02:01 -0600

    Woohoo init stuff

---
+++ gcc/ShadowCore.h
@@ -0,0 +1,28 @@
+//Copyright (c) 77Z 2021
+
+#ifndef _SHADOWCORE_
+#define _SHADOWCORE_
+
+#include <string>
+
+//DLL Support
+#ifdef WINDOWSDLL
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT  
+#endif
+
+//General Print
+EXPORT void print(std::string text); //C++ String Style Print
+EXPORT void println(std::string text);
+
+EXPORT void printa(char text[]); //C Char Array Style Print
+EXPORT void printlna(char text[]);
+
+//Get system spec
+EXPORT int getSysMemoryUsage(); //Get System Memory Usage
+
+//Micellaneous
+EXPORT void aboutCore(); //Info about ShadowCore
+
+#endif /* _SHADOWCORE_ */
---
+++ gcc/main.cpp
@@ -0,0 +1,48 @@
+#include "vlib.h"
+#include "version.h"
+
+#ifdef _WIN32
+
+#include <Windows.h>
+
+#endif
+
+#include <iostream>
+#include <string>
+#include <ctime>
+
+EXPORT void print(std::string text) {
+	std::cout << text;
+}
+EXPORT void println(std::string text) {
+	std::cout << text << std::endl;
+}
+
+EXPORT void printa(char text[]) {
+	std::cout << text;
+}
+EXPORT void printlna(char text[]) {
+	std::cout << text << std::endl;
+}
+
+EXPORT int getSysMemoryUsage() {
+#ifdef _WIN32
+	MEMORYSTATUSEX statex;
+	statex.dwLength = sizeof(statex);
+	GlobalMemoryStatusEx(&statex);
+
+	return statex.dwMemoryLoad;
+#endif
+#ifdef linux
+	return 0;
+#endif
+}
+
+time_t now = time(0);
+char* dt = ctime(&now);
+
+EXPORT void aboutCore() {
+	print("ShadowCore Version "); println(CORE_VERSION);
+	print("Time of Run: "); println(dt);
+	print("Time of Compile: "); print(__DATE__); print(" "); println(__TIME__);
+}
